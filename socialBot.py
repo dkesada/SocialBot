23849b22c9d76671bdf381adbce0dc14b4f4f5aa
David Quesada <dkesada@gmail.com>
2017-02-22 18:51:18 +0100

    First commit.

---
+++ socialBot.py
@@ -0,0 +1,49 @@
+#! /usr/bin/python
+#-*. coding: utf-8 -*-
+# David Quesada López
+import sys
+import time
+import threading
+from Queue import Queue
+import telepot
+import telepot.helper
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+from telepot.delegate import (
+    per_chat_id, per_callback_query_origin, create_open, pave_event_space)
+
+
+# One UserHandler created per chat_id. May be useful for sorting out users
+class UserHandler(telepot.helper.ChatHandler):
+    def __init__(self, *args, **kwargs):
+        super(UserHandler, self).__init__(*args, **kwargs)
+
+    def on_chat_message(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        self.sender.sendMessage(
+            'Pulsa START para buscar una partida.',
+            reply_markup=InlineKeyboardMarkup(
+                inline_keyboard=[[
+                    InlineKeyboardButton(text='START', callback_data='start'),
+                ]]
+            )
+        )
+
+        self.close()  # Dejas que Game se haga cargo de las partidas
+
+# One ButtonHandler
+class ButtonHandler(telepot.helper.CallbackQueryOriginHandler):
+    def __init__(self, *args, **kwargs):
+        super(ButtonHandler, self).__init__(*args, **kwargs)
+
+
+
+TOKEN = '255866015:AAFvI3sUR1sOFbeDrUceVyAs44KlfKgx-UE'
+
+bot = telepot.DelegatorBot(TOKEN, [
+    pave_event_space()(
+        per_chat_id(), create_open, UserHandler, timeout=10),
+    pave_event_space()(
+        per_callback_query_origin(), create_open, ButtonHandler, timeout=30),
+])
+
+bot.message_loop()
