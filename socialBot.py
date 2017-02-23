c06dcbd6f8eb314dfbe8cb340ff3d153cfebeef8
mateteh <mateogar@ucm.es>
2017-02-23 20:01:47 +0100

    button Location and manage conexions

--- socialBot.py
+++ socialBot.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python
 #-*. coding: utf-8 -*-
+#authors: David Quesada López y Mateo García Fuentes
 
 import sys
 import time
@@ -8,6 +9,7 @@ from Queue import Queue
 import telepot
 import telepot.helper
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 from telepot.delegate import (
     per_chat_id, per_callback_query_origin, create_open, pave_event_space)
 
@@ -20,18 +22,44 @@ los que tengamos algún dato. O dos colecciones de mongo o todo en la misma, por
 join si hubiese.
 Vamos a intentar desde el principio poner todo el código en inglés, y eso que nos ahorramos
 para luego.
-
 Para mirar cosas del telepot la mejor documentacion es esta:
-
 http://telepot.readthedocs.io/en/latest/reference.html
-
 Ahí se puede buscar las funciones en concreto, lo que devuelven, lo que necesitan,...
 No está muy allá en algunos casos pero bueno, menos es nada. Entre eso y los ejemplos del tio
 va bien.
-
-
 """
 
+clients = {}
+q = Queue()
+# The manager takes everything about creating conexions and to finish them
+class Manager(threading.Thread):
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.num = 0
+        self.ids = [1,2]
+    
+    def run(self):
+        while(True):
+            message = self.queue.get()
+            if message[0] is 'pair':
+                self.request(message[1])
+            elif message[0] is 'delPair':
+                del clients[message[1]]
+            time.sleep(3)
+            
+    def request(self, x):
+        if self.num == 1 and self.ids[0] != x:
+            self.ids[1] = x
+            q1 = Queue()
+            q1.put([0,' '])
+            q2 = Queue()
+            partidas[self.ids[0]] = [self.ids[1],'x',q1]
+            partidas[self.ids[1]] = [self.ids[0],'o',q2]
+            self.num = 0
+        else:
+            self.ids[0] = x
+            self.num = 1
 
 # One UserHandler created per chat_id. May be useful for sorting out users
 # Handles chat messages, we should sort out with telepot.glance what to do with the
@@ -41,12 +69,14 @@ class UserHandler(telepot.helper.ChatHandler):
         super(UserHandler, self).__init__(*args, **kwargs)
 
     def on_chat_message(self, msg):
-        content_type, chat_type, chat_id = telepot.glance(msg,flavor='chat')
-        # Some content_type values: text, location, photo
-        
-        if content_type == 'text':
-            
-        elif content_type == 'location':
+		content_type, chat_type, chat_id = telepot.glance(msg,flavor='chat')
+		if content_type == 'text':	
+			markup = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Location', request_location=True)],])
+			 
+		bot.sendMessage(chat_id, 'Share your location', reply_markup=markup)
+		# Some content_type values: text, location, photo
+		#			
+        #elif content_type == 'location':
         
 
 # One ButtonHandler created per message that has a button pressed.
@@ -60,10 +90,13 @@ class ButtonHandler(telepot.helper.CallbackQueryOriginHandler):
         query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
         bot.answerCallbackQuery(query_id)
 
-
+            
 # El token si quieres puedo ponerlo para que lo pongamos por consola como argumento al 
 # lanzar el bot
 TOKEN = '255866015:AAFvI3sUR1sOFbeDrUceVyAs44KlfKgx-UE'
+manage = Manager(q)
+manage.setDaemon = True
+manage.start()
 
 bot = telepot.DelegatorBot(TOKEN, [
     pave_event_space()(
