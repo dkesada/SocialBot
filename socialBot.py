8a438453c672b9ad50d5575ff4dff6f7fcdac25d
mateteh <mateogar@ucm.es>
2017-04-29 19:19:45 +0200

    Add more tranlate methods

--- socialBot.py
+++ socialBot.py
@@ -133,18 +133,15 @@ class ButtonHandler(telepot.helper.CallbackQueryOriginHandler):
 					self.state -= 1;
 					steps.saveStep(from_id, self.state)
 					self.editor.editMessageReplyMarkup(reply_markup=None)
-					bot.sendMessage(self.chat_id, 'What do you want to do?', reply_markup=keyboards.optionsKeyboard(self.loc))
+					bot.sendMessage(self.chat_id, translate.whatWant(self.language), reply_markup=keyboards.optionsKeyboard(self.loc))
 							
 		elif steps.step(self.state) == "Settings":
 			if query_data == "language":
-				self.editor.editMessageText("Choose your language", reply_markup=keyboards.languages)
+				self.editor.editMessageText(translate.chooseLang(self.language), reply_markup=keyboards.languages)
 			elif query_data == "parameters":
-				self.editor.editMessageText("Choose the parameters for your query (radius, open now, etc.).", reply_markup=keyboards.parameters)
-			elif query_data == "sback":
-				text = "From here you can change the bot's settings. On Choose language you can change the bot's language. "
-				text += "On Choose parameters you can change the radius of the establishments you want to go to, if you want "
-				text += "the bot show only establishments that are open or the price of them."
-				self.editor.editMessageText(text, reply_markup=keyboards.settings)
+				self.editor.editMessageText(translate.choooseParam(self.language), reply_markup=keyboards.parameters)
+			elif query_data == "sback":				
+				self.editor.editMessageText(translate.settings(self.language), reply_markup=keyboards.settings)
 			elif query_data == "radius":
 				self.editor.editMessageText("Choose one of the distances which you want to set the radius of the establishments that you are looking for. The distance is in meters", reply_markup=keyboards.radius)
 			elif query_data == "open":
--- translate.py
+++ translate.py
@@ -76,3 +76,38 @@ def langChanged(lang){
 	else:
 		return "Language changed"
 }
+
+
+def whatWant(lang){
+	if lang == "Español":
+		return "¿Qué quieres hacer?"
+	else:
+		return "What do you want to do?"
+}
+
+def chooseLang(lang){
+	if lang == "Español":
+		return "Escoge tu idioma"
+	else:
+		return "Choose your language"
+}
+
+def choooseParam(lang){
+	if lang == "Español":
+		return "Escoge los parámetros de tu consulta (radio, abierto ahora, etc.):"
+	else:
+		return "Choose the parameters for your query (radius, open now, etc.)."
+}
+
+def choooseParam(lang){
+	if lang == "Español":
+		text = "Desde aquí puedes cambiar los ajustes del bot. En Escoge idioma puedes cambiar el idioma del bot. "
+		text += "En Escoge los parámetros puedes cambiar el radio de los locales a los que quieres ir, si quieres "
+		text += "el bot puede mostarte solo los establecimientos abiertos."
+		return tex
+	else:
+		text = "From here you can change the bot's settings. On Choose language you can change the bot's language. "
+		text += "On Choose parameters you can change the radius of the establishments you want to go to, if you want "
+		text += "the bot can show only locals that are open."
+		return text
+}
